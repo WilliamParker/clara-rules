a85bd8cef545f4d9e9f4547ef3e46a576a42a7c7
William Parker <wparker30@yahoo.com>
2016-07-22 12:33:13 -0500

    test 3

--- src/test/clojure/clara/generative/test_accum.clj
+++ src/test/clojure/clara/generative/test_accum.clj
@@ -44,6 +44,7 @@
                                    expected-temp-hist (frequencies [{:?history (->TemperatureHistory ["MCI" [11 19]])}
                                                                     {:?history (->TemperatureHistory ["ORD" [1]])}])]
                                (= actual-temp-hist expected-temp-hist)))]
+      
       (doseq [permutation operation-permutations
               :let [session (gen/session-run-ops empty-session operations)]]
         (is (expected-output? session permutation)
@@ -76,4 +77,81 @@
                  (into [] permutation)
                  "Output was: "
                  (into [] output)))))))
-        
+
+(deftest test-min-accum-with-binding-groups
+  (let [coldest-rule (dsl/parse-rule [[?coldest-temp <- (acc/min :temperature :returns-fact true)
+                                       :from [ColdAndWindy (= ?w windspeed)]]]
+                                     (insert! (->Cold (:temperature ?coldest-temp))))
+        cold-query (dsl/parse-query [] [[?c <- Cold]])
+
+        empty-session (mk-session [coldest-rule cold-query] :cache false)
+
+        operations [{:type :insert
+                     :facts [(->ColdAndWindy 10 20)]}
+                    {:type :insert
+                     :facts [(->ColdAndWindy 5 20)]}
+                    {:type :retract
+                     :facts [(->ColdAndWindy 5 20)]}
+                    {:type :insert
+                     :facts [(->ColdAndWindy 20 20)]}
+                    {:type :insert
+                     :facts [(->ColdAndWindy 0 30)]}
+                    {:type :fire}
+                    {:type :fire}]
+
+        operation-permutations (gen/ops->permutations operations {})]
+
+    (doseq [permutation operation-permutations
+            :let [session (gen/session-run-ops empty-session operations)
+                  output (query session cold-query)]]
+      (is (= (frequencies output)
+             {{:?c (->Cold 0)} 1
+              {:?c (->Cold 10)} 1})
+          (str "The minimum cold temperatures per windspeed are not correct for permutation: "
+               \newline
+               (into [] permutation)
+               \newline
+               "The output was: "
+               (into [] output))))))
+
+(deftest test-coldest-cold
+  (let [coldest-rule (dsl/parse-rule [[?coldest <- (acc/min :temperature) :from [Cold]]]
+                                     (insert! (->Temperature ?coldest "MCI")))
+        temp-query (dsl/parse-query [] [[Temperature (= ?t temperature)]])
+
+        empty-session (mk-session [coldest-rule temp-query] :cache false)]
+
+    (doseq [temp-1 (range 5)
+            temp-2 (range 5)
+            temp-3 (range 5)
+
+            :let [operations [{:type :insert
+                               :facts [(->Cold temp-1)]}
+                              {:type :insert
+                               :facts [(->Cold temp-2)]}
+                              {:type :insert
+                               :facts [(->Cold temp-3)]}
+                              {:type :retract
+                               :facts [(->Cold temp-3)]}]]
+
+            permutation (map #(concat % [{:type :fire}])
+                             (gen/ops->permutations operations {}))
+
+            :let [session (gen/session-run-ops empty-session permutation)
+                  output (query session temp-query)]]
+      (is (= output
+             [{:?t (min temp-1 temp-2)}])
+          (str "Did not find the correct minimum temperature for permutation: "
+               \newline
+               (into [] permutation)
+               \newline
+               "Output was: "
+               \newline
+               (into [] output))))))
+
+                  
+                               
+                  
+      
+               
+                  
