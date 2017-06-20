76ad02724fa669035a2e2c78b9f50f647cf26997
William Parker <wparker30@yahoo.com>
2017-06-20 11:18:55 -0500

    test-unconditional-insert-all

--- src/test/clojure/clara/test_rules.clj
+++ src/test/clojure/clara/test_rules.clj
@@ -1649,38 +1649,6 @@
     (is (= #{{:?c 10}}
            (set (query session cold-lousy-query))))))
 
-(deftest test-unconditional-insert-all
-  (let [rule-output (atom nil)
-        ;; Insert a new fact and ensure it exists.
-        cold-lousy-rule (dsl/parse-rule [[Temperature (< temperature 20) (= ?t temperature)]]
-                                  (insert-all-unconditional! [(->Cold ?t) (->LousyWeather)]))
-
-        cold-query (dsl/parse-query [] [[Cold (= ?c temperature)]])
-
-        lousy-query (dsl/parse-query [] [[?l <- LousyWeather]])
-
-        session (-> (mk-session [cold-lousy-rule cold-query lousy-query])
-                    (insert (->Temperature 10 "MCI"))
-                    (fire-rules))
-
-        retracted-session (-> session
-                              (retract (->Temperature 10 "MCI"))
-                              fire-rules)]
-
-    (is (= #{{:?c 10}}
-           (set (query session cold-query))))
-
-    (is (= #{{:?l (->LousyWeather)}}
-           (set (query session lousy-query))))
-
-    ;; The derived fact should continue to exist after a retraction
-    ;; since we used an unconditional insert.
-    (is (= #{{:?c 10}}
-           (set (query retracted-session cold-query))))
-
-    (is (= #{{:?l (->LousyWeather)}}
-           (set (query retracted-session lousy-query))))))
-
 (deftest test-insert-and-retract-multi-input
   (let [rule-output (atom nil)
         ;; Insert a new fact and ensure it exists.
